a769b6a79badeba342042cbf8ec0ef64ef13be54
Steve Harlan <StevenHarlan@gmail.com>
2016-11-04 15:31:08 -0700

    package.json updated with dependencies; gitignore added node_modules

---
+++ main.js
@@ -0,0 +1,16 @@
+function main() {
+  console.log('hello')
+  getAllCaptials()
+  getElevationsFrom()
+};
+
+
+function getAllCaptials(){
+  console.log('all capitals')
+}
+
+function getElevationsFrom(coordinates){
+  console.log('coordinates')
+}
+
+main()
